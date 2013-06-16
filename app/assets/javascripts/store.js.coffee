427df97ad2a0a1fd8cf3bdd06804d0f3fc15d58f
lbs <lbspen@gmail.com>
2013-06-15 20:28:40 -0700

    finished raffler app

---
+++ app/assets/javascripts/store.js.coffee
@@ -0,0 +1,4 @@
+Raffler.Store = DS.Store.extend
+  revision: 4
+
+DS.RESTAdapter.configure("plurals", entry: "entries")
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = '489531f7839d5e11a34a8f1eb1cd51e19cdb1d7b7de33d11abf912d646fff0ac98c7762e9816a734c078175ab05c3e1cf9b3bb2a406b5c51de1304fadec3a5ec'
